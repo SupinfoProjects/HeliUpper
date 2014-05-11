c8326c3c1d1ff63e337994e70f1ae1d27048a670
Wassim Dhif <wassimdhif@gmail.com>
2014-05-11 15:56:10 +0200

    Version 1.0
    Signed-off-by: Wassim Dhif <wassimdhif@gmail.com>

--- Headers/ennemy.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENNEMY_H_INCLUDED
-#define ENNEMY_H_INCLUDED
-#include "..\Headers\enums.h"
-
-
-
-
-#endif // ENNEMY_H_INCLUDED
--- Headers/sprite.h
+++
@@ -1,7 +0,0 @@
-#ifndef SPRITE_H_INCLUDED
-#define SPRITE_H_INCLUDED
-#include <SDL/SDL.h>
-
-
-
-#endif // SPRITE_H_INCLUDED
